6303886750124a507703694f2ae55f4e43dc8670
Iva.Melnikov1 <Iva.Melnikov1@x5.ru>
2022-04-09 22:04:09 +0300

    leetcode WordBreakProblem

--- src/main/java/com/leetcode/WordBreakProblem.java
+++ src/main/java/com/leetcode/WordBreakProblem.java
@@ -1,19 +1,65 @@
 package com.leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class WordBreakProblem {
     public static void main(String[] args) {
-        System.out.println(new SimpleSolution().wordBreak("leetcode", new ArrayList<>() {{
-            add("leet");
-            add("code");
+        System.out.println(new BFSSolution().wordBreak("applepenapple", new ArrayList<>() {{
+            add("apple");
+            add("pen");
+            add("bbb");
+            add("bbbb");
         }}));
     }
 
     /**
-     * TODO не правильно понял задачу переделать
+     * Смысл задачи следующий
+     * необходимо понять что строка изначально делиться на слова из словаря
      *
+     * пример applepenapple, словарь apple pen
+     * 0, 5 - apple
+     * 5, 8 - pen
+     * 8, 13 - apple
+     *
+     * пример applepen, словарь apples, pen
+     * вернет false
+     * так как по факту у нас никогда не будет вхождения apple
+     */
+    static class BFSSolution {
+        public boolean wordBreak(String s, List<String> wordDict) {
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(0);
+
+            int len = s.length();
+            boolean[] visited = new boolean[len];
+            while (!queue.isEmpty()) {
+                System.out.printf("Queue: %s %n", queue);
+                int start = queue.poll();
+                System.out.printf("Start: %s %n", start);
+                if (!visited[start]) {
+                    for (int end = start + 1; end <= len; end++) {
+                        String segment = s.substring(start, end);
+                        System.out.printf("Start: %s, End: %s, Segment: %s, Queue: %s %n", start, end, segment, queue);
+                        if (wordDict.contains(segment)) {
+                            if (end == len)
+                                return true;
+                            else
+                                queue.add(end);
+                        }
+                    }
+                    visited[start] = true;
+                }
+            }
+            return false;
+        }
+    }
+
+    /**
+     * TODO не правильно понял задачу переделать
+     * <p>
      * Input:
      * "bb"
      * ["a","b","bbb","bbbb"]
