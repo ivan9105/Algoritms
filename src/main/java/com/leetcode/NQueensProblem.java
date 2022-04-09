d5f4cda356e780125e907b697df86b9d331577f4
Iva.Melnikov1 <Iva.Melnikov1@x5.ru>
2022-04-09 22:39:22 +0300

    leetcode NQueensProblem

---
+++ src/main/java/com/leetcode/NQueensProblem.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+import java.util.List;
+
+public class NQueensProblem {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solveNQueens());
+    }
+
+    /**
+     * Input: n = 4
+     * Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+     */
+    static class Solution {
+        public List<List<String>> solveNQueens(int n) {
+
+        }
+    }
+}
